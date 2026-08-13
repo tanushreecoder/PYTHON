d0acc8f56c3ccd34ea7224d8291b9064d0e2472d
tanushreecoder <tanushree.coder@gmail.com>
2026-08-13 19:27:23 +0600

    Update Tuple Operations.py

--- Tuple Operations.py
+++ Tuple Operations.py
@@ -7,3 +7,33 @@ for name, mark in students:
     print(name, mark)
 results = sorted(students, key=lambda x:x[1], reverse=True)
 print(results)
+names = ("Tanushree", "Milly", "David")
+marks = (90, 92, 97)
+students = tuple(zip(names, marks))
+print(students)
+def palind(r):
+    e = len(r)-1
+    s = 0
+    while(s<e):
+        if(r[s]!=r[e]):
+            return False
+        s += 1
+        e -= 1
+        return True
+r = (1, 2, 3, 3, 2, 1)
+if (palind(r)):
+    print("The tuple is Flip Flop")
+else:
+    print("The tuple is not Flip Flop")
+weather = (1, 0, 0, 0, 1, 1, 0)
+sunny = 0
+rainy = 0
+for i in range(0, 7):
+    if(weather[i]==0):
+        rainy += 1
+    else:
+        sunny += 1
+if (sunny>rainy):
+    print("Good Weather")
+else:
+    print("Bad Weather")
